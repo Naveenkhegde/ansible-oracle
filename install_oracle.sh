232286fa8cd4cf3428e6dc9e7c26786ba93be223
Naveenkhegde <naveenkhegde@gmail.com>
2024-11-07 11:38:19 +0530

    deleting unnecessary files

--- install_oracle.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-date
-ansible-playbook playbooks/install_oracle.yml
-date
-ansible-playbook playbooks/create_db.yml
-date
--- install_oracle_with_asm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-date
-ansible-playbook playbooks/install_grid.yml
-date
-ansible-playbook playbooks/create_db_asm.yml
-date
